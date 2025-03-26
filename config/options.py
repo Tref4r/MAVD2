f6c37304ac78af6522908edd3dcfe4892a66deb7
Tref4r <113423456+Tref4r@users.noreply.github.com>
2025-03-26 21:46:15 +0700

    Update options.py

--- config/options.py
+++ config/options.py
@@ -19,7 +19,7 @@ parser.add_argument('--init_path', type = str, default = 'saved_models/init_mode
 parser.add_argument('--model_file', type = str, default = "model_{}.pkl".format(seed), help = 'the path of pre-trained model file')
 
 parser.add_argument('--lr', type = float, default = 0.0001, help = 'learning rates')
-parser.add_argument('--batch_size', type = int, default = 64)
+parser.add_argument('--batch_size', type = int, default = 128)
 parser.add_argument('--seed', type = int, default = 500, help = 'random seed (-1 for no manual seed)')
 parser.add_argument('--workers', default=8, help='number of workers in dataloader')
 parser.add_argument('--num_steps', default=1000, help='number of epochs to train for')
@@ -27,11 +27,9 @@ parser.add_argument('--num_steps', default=1000, help='number of epochs to train
 parser.add_argument('--rgb-list', default='./list/video_train.list', help='list of rgb features')
 parser.add_argument('--flow-list', default='./list/flow_train.list', help='list of flow features')
 parser.add_argument('--audio-list', default='./list/audio_train.list', help='list of audio features')
-parser.add_argument('--pose-list', default='./list/pose_train.list', help='list of pose features')
 parser.add_argument('--test-rgb-list', default='./list/video_test.list', help='list of test rgb features ')
 parser.add_argument('--test-flow-list', default='./list/flow_test.list', help='list of test flow features')
 parser.add_argument('--test-audio-list', default='./list/audio_test.list', help='list of test audio features')
-parser.add_argument('--test-pose-list', default='./list/pose_test.list', help='list of test audio features')
 
 parser.add_argument('--max_seqlen', type=int, default=200, help='maximum sequence length during training')
 parser.add_argument('--gt', default='./list/gt.npy', help='file of ground truth ')
